ef0d746d72a8cea098973fa595b67b097e8963fb
Marion Newlevant <marion.newlevant@gmail.com>
2020-12-23 15:03:11 -0800

    Version 3.0.4 - Snitch vs Two Factor Authentication

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 3.0.4 - 2020.12.23
+### Fixed
+- Fixed problem with Snitch interfering with Two Factor Authentication
+
 ## 3.0.3 - 2020.09.24
 ### Fixed
 - Fix casing on snitch namespace in assetbundles
--- src/Snitch.php
+++ src/Snitch.php
@@ -10,16 +10,17 @@
 
 namespace marionnewlevant\snitch;
 
+use craft\services\Plugins;
 use marionnewlevant\snitch\services\Collision as Collision;
 use marionnewlevant\snitch\assetbundles\snitch\SnitchAsset;
 use marionnewlevant\snitch\models\Settings;
 
 use Craft;
 use craft\base\Plugin;
-// use craft\events\ElementEvent;
-// use craft\services\Elements;
+use yii\base\Event;
 
-// use yii\base\Event;
+use born05\twofactorauthentication\Plugin as TwoFactorAuth;
+
 
 /**
  * Craft plugins are very much like little applications in and of themselves. We’ve made
@@ -81,20 +82,41 @@ class Snitch extends Plugin
         parent::init();
         self::$plugin = $this;
 
+        $request = Craft::$app->getRequest();
+        $response = Craft::$app->getResponse();
+
+        if (!$this->isInstalled || $request->getIsConsoleRequest()) {
+            return;
+        }
+
         $this->setComponents([
             'collision' => Collision::class,
         ]);
 
-        if (Craft::$app->getRequest()->getIsCpRequest() && !Craft::$app->getRequest()->getIsAjax()) {
-            // Register our asset bundle
-            Craft::$app->getView()->registerAssetBundle(SnitchAsset::class);
-            // on save, remove any collision for this element.
-            // I used to do this, but it ties me to
-            // specific classes. Instead, just rely on the record timing out.
-            // Event::on(Elements::class, Elements::EVENT_AFTER_SAVE_ELEMENT, function(ElementEvent $event) {
-            //     $elementId = $event->element->id;
-            //     $this->collision->remove($elementId, 'element');
-            // });
+        if ($request->getIsCpRequest()
+            && !$request->getIsAjax()
+            && !Craft::$app->getUser()->getIsGuest()
+        ) {
+            // delay this check until plugins loaded...
+            Event::on(Plugins::class, Plugins::EVENT_AFTER_LOAD_PLUGINS, function () {
+                $user = Craft::$app->getUser();
+                // TwoFactorAuth - either not there, or not turned on for this user, or we have passed that hurdle
+                if (!TwoFactorAuth::$plugin
+                    || !TwoFactorAuth::$plugin->verify->isEnabled($user->getIdentity())
+                    || TwoFactorAuth::$plugin->verify->isVerified($user->getIdentity())
+                )
+                {
+                    // Register our asset bundle
+                    Craft::$app->getView()->registerAssetBundle(SnitchAsset::class);
+                    // on save, remove any collision for this element.
+                    // I used to do this, but it ties me to
+                    // specific classes. Instead, just rely on the record timing out.
+                    // Event::on(Elements::class, Elements::EVENT_AFTER_SAVE_ELEMENT, function(ElementEvent $event) {
+                    //     $elementId = $event->element->id;
+                    //     $this->collision->remove($elementId, 'element');
+                    // });
+                }
+            });
         }
 
         Craft::info(
